f7d2c0044ea6df8dd3d0d7f3dfb8edbcfbe6b425
pmmrkumaradasa@students.nsbm.ac.lk <pmmrkumaradasa@students.nsbm.ac.lk>
2021-06-20 16:50:27 +0530

    Added new window (Product Manager)

---
+++ Stockpro/Product Manager.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Stockpro
+{
+    partial class Product_Manager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Product_Manager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Product_Manager";
+            this.Text = "Product_Manager";
+            this.Load += new System.EventHandler(this.Product_Manager_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Stockpro/Product Manager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stockpro
+{
+    public partial class Product_Manager : Form
+    {
+        public Product_Manager()
+        {
+            InitializeComponent();
+        }
+
+        private void Product_Manager_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
