68b06de1f26481584f8174a54e435391707eb52e
pmmrkumaradasa@students.nsbm.ac.lk <pmmrkumaradasa@students.nsbm.ac.lk>
2021-06-21 11:34:53 +0530

    Added items to the window Product_Manager

--- Stockpro/Product Manager.Designer.cs
+++ Stockpro/Product Manager.Designer.cs
@@ -29,20 +29,360 @@ namespace Stockpro
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAddId = new System.Windows.Forms.TextBox();
+            this.txtAddName = new System.Windows.Forms.TextBox();
+            this.txtAddPrice = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtUpdatePrice = new System.Windows.Forms.TextBox();
+            this.txtUpdateName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.cmbUpdateId = new System.Windows.Forms.ComboBox();
+            this.cmbDeleteId = new System.Windows.Forms.ComboBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.lblSearchId = new System.Windows.Forms.Label();
+            this.lblSearchName = new System.Windows.Forms.Label();
+            this.lblSearchPrice = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add new product";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Price";
+            // 
+            // txtAddId
+            // 
+            this.txtAddId.Location = new System.Drawing.Point(69, 64);
+            this.txtAddId.Name = "txtAddId";
+            this.txtAddId.Size = new System.Drawing.Size(226, 20);
+            this.txtAddId.TabIndex = 4;
+            // 
+            // txtAddName
+            // 
+            this.txtAddName.Location = new System.Drawing.Point(69, 90);
+            this.txtAddName.Name = "txtAddName";
+            this.txtAddName.Size = new System.Drawing.Size(226, 20);
+            this.txtAddName.TabIndex = 5;
+            // 
+            // txtAddPrice
+            // 
+            this.txtAddPrice.Location = new System.Drawing.Point(69, 118);
+            this.txtAddPrice.Name = "txtAddPrice";
+            this.txtAddPrice.Size = new System.Drawing.Size(226, 20);
+            this.txtAddPrice.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(31, 144);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(264, 23);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add to database";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(31, 321);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(264, 23);
+            this.btnUpdate.TabIndex = 15;
+            this.btnUpdate.Text = "Update database";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // txtUpdatePrice
+            // 
+            this.txtUpdatePrice.Location = new System.Drawing.Point(94, 295);
+            this.txtUpdatePrice.Name = "txtUpdatePrice";
+            this.txtUpdatePrice.Size = new System.Drawing.Size(201, 20);
+            this.txtUpdatePrice.TabIndex = 14;
+            // 
+            // txtUpdateName
+            // 
+            this.txtUpdateName.Location = new System.Drawing.Point(94, 267);
+            this.txtUpdateName.Name = "txtUpdateName";
+            this.txtUpdateName.Size = new System.Drawing.Size(201, 20);
+            this.txtUpdateName.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 298);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "New Price";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(32, 270);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "New Name";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(70, 244);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "ID";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Lucida Console", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(27, 208);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(214, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Update existing product";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Lucida Console", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(326, 31);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(169, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Search for product";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(329, 64);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(226, 20);
+            this.txtSearch.TabIndex = 17;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(561, 62);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(100, 23);
+            this.btnSearch.TabIndex = 18;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Lucida Console", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(326, 208);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(133, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Delete product";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(326, 244);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(18, 13);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "ID";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(561, 239);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 23);
+            this.btnDelete.TabIndex = 22;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // cmbUpdateId
+            // 
+            this.cmbUpdateId.FormattingEnabled = true;
+            this.cmbUpdateId.Location = new System.Drawing.Point(94, 241);
+            this.cmbUpdateId.Name = "cmbUpdateId";
+            this.cmbUpdateId.Size = new System.Drawing.Size(107, 21);
+            this.cmbUpdateId.TabIndex = 23;
+            // 
+            // cmbDeleteId
+            // 
+            this.cmbDeleteId.FormattingEnabled = true;
+            this.cmbDeleteId.Location = new System.Drawing.Point(350, 241);
+            this.cmbDeleteId.Name = "cmbDeleteId";
+            this.cmbDeleteId.Size = new System.Drawing.Size(205, 21);
+            this.cmbDeleteId.TabIndex = 24;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(329, 93);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(18, 13);
+            this.label12.TabIndex = 25;
+            this.label12.Text = "ID";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(329, 121);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(35, 13);
+            this.label13.TabIndex = 26;
+            this.label13.Text = "Name";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(329, 149);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(31, 13);
+            this.label14.TabIndex = 27;
+            this.label14.Text = "Price";
+            // 
+            // lblSearchId
+            // 
+            this.lblSearchId.AutoSize = true;
+            this.lblSearchId.Location = new System.Drawing.Point(381, 93);
+            this.lblSearchId.Name = "lblSearchId";
+            this.lblSearchId.Size = new System.Drawing.Size(41, 13);
+            this.lblSearchId.TabIndex = 28;
+            this.lblSearchId.Text = "label15";
+            // 
+            // lblSearchName
+            // 
+            this.lblSearchName.AutoSize = true;
+            this.lblSearchName.Location = new System.Drawing.Point(381, 121);
+            this.lblSearchName.Name = "lblSearchName";
+            this.lblSearchName.Size = new System.Drawing.Size(41, 13);
+            this.lblSearchName.TabIndex = 29;
+            this.lblSearchName.Text = "label16";
+            // 
+            // lblSearchPrice
+            // 
+            this.lblSearchPrice.AutoSize = true;
+            this.lblSearchPrice.Location = new System.Drawing.Point(381, 149);
+            this.lblSearchPrice.Name = "lblSearchPrice";
+            this.lblSearchPrice.Size = new System.Drawing.Size(41, 13);
+            this.lblSearchPrice.TabIndex = 30;
+            this.lblSearchPrice.Text = "label17";
             // 
             // Product_Manager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(683, 385);
+            this.Controls.Add(this.lblSearchPrice);
+            this.Controls.Add(this.lblSearchName);
+            this.Controls.Add(this.lblSearchId);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.cmbDeleteId);
+            this.Controls.Add(this.cmbUpdateId);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtUpdatePrice);
+            this.Controls.Add(this.txtUpdateName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtAddPrice);
+            this.Controls.Add(this.txtAddName);
+            this.Controls.Add(this.txtAddId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Product_Manager";
             this.Text = "Product_Manager";
             this.Load += new System.EventHandler(this.Product_Manager_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAddId;
+        private System.Windows.Forms.TextBox txtAddName;
+        private System.Windows.Forms.TextBox txtAddPrice;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtUpdatePrice;
+        private System.Windows.Forms.TextBox txtUpdateName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ComboBox cmbUpdateId;
+        private System.Windows.Forms.ComboBox cmbDeleteId;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label lblSearchId;
+        private System.Windows.Forms.Label lblSearchName;
+        private System.Windows.Forms.Label lblSearchPrice;
     }
 }
